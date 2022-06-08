8595f982a841c3cb671be2a79a39c6e4040f4c81
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-07 20:04:16 -0700

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: jack_bauer() prints every minute of the day 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	jack_bauer();
+
+	return (0);
+}
