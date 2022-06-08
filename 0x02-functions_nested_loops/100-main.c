f3a2928c367a8443102b037b5a711610b1f1dd9b
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-07 21:36:36 -0700

    prints the n times table

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: print_times_table prints the n times table. 
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+
+	return (0);
+}
