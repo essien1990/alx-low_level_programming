1f738fc59877f3841bf4201374fdc27ecd1f290c
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-09 10:28:30 -0700

    print numbers(0-9)

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,11 +3,9 @@
 /**
  * print_numbers - function that prints number 0-9
  *
- * @i: variable for the numbers
- *
  * Description: Prints numbers from 0 to 9
  *
- * Return: returns nothing 
+ * Return: returns nothing
  */
 
 void print_numbers(void)
