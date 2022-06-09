4a83115ebe591b92a9ea744057e55d7d690423fb
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-09 10:13:21 -0700

    product of numbes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers and returns a result
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Description: Multiplies two numbers and returns  a result
+ *
+ * Return: Multiplication of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
