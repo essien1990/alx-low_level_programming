d0c2b8716d5da35cb1d8d3f1b96ba006efc3fd44
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-09 14:01:00 -0700

    print numbers2

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers
+ *
+ * @n: number for input
+ *
+ * Description: Prints numbers
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(int n)
+{
+	long length, result, a, tmp, expo;
+
+	result = n;
+	expo = length = 1;
+
+	if (result < 0)
+	{
+		result = result * -1;
+		_putchar('-');
+	}
+
+	tmp = result;
+	while (tmp >= 10)
+	{
+		length++;
+		tmp = tmp / 10;
+	}
+
+	for (a = 1; a  < length; a++)
+		expo = expo * 10;
+
+	while (expo > 1)
+	{
+		_putchar((result / expo) % 10 + '0');
+		expo = expo / 10;
+	}
+	_putchar(result % 10 + '0');
+}
