1c20e3c0119671a063fdc89837b2e0eca21e42a1
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-13 18:59:26 -0700

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,7 +7,7 @@
 
 void print_rev(char *s)
 {
-	int n;
+	int n = 0;
 
 	while (s[n] != '\0')
 	{
@@ -15,9 +15,10 @@ void print_rev(char *s)
 	}
 	n--;
 
-	while (s[n] != '\0')
+	while (n >= 0)
 	{
-		_putchar(s[n--]);
+		_putchar(s[n]);
+		n--;
 	}
 	_putchar('\n');
 }
