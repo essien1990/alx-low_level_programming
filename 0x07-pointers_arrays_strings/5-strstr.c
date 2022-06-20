021c92b912e53d8e0ca649efb11c94e01ae6830b
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-20 07:02:44 -0700

    Task 05

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,47 +1,38 @@
 #include "main.h"
 
 /**
- * _strncmp - compares two strings within b bytes.
- * @str1: string to check
- * @str2: string to check from
- * @b: number of bytes to check
- * Return: 0 if strings are the same
- * any other number if they are different.
- */
-
-int _strncmp(char *str1, char *str2, unsigned int b)
-{
-	unsigned int i;
-
-	for (i = 0; i <= b && *str1 == *str2 && *str1 != '\0' && *str2 != '\0'; i++)
-	{
-		str1++;
-		str2++;
-	}
-	return (i - b);
-}
-
-/**
  * _strstr - locates a substring
  * @haystack: string to look though
  * @needle: substring to find
- * Return: pointer to string with result of search
+ * Return: pointer to initial segment of haystack
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	unsigned int length;
+	int a, b, start;
+	int len = 0;
 
-	/*Get the length of needle for strncmp*/
-	length = 0;
+	a = 0;
+	b = 0;
+	start = 0;
+
 	while (needle[len] != '\0')
-		length++;
-	/*compare substrings*/
-	while (*haystack != '\0')
 	{
-		if (_strncmp(haystack, needle, length) == 0)
-			return (haystack);
-		haystack++;
+		len++;
 	}
-	return (NULL);
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; b < len && haystack[a] == needle[b]; b++, a++)
+		{
+			if (b == 0)
+			{
+				start = a;
+			}
+			if (b == len - 1)
+			{
+				return (haystack + start);
+			}
+		}
+	}
+	return (0);
 }
