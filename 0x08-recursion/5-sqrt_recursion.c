5730871b34242857d08f488618e1719b19297864
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-06-22 05:01:22 -0700

    Task 05

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * check - checks the input number from x to the base
+ * @x: number is squared and compared against base
+ * @base: base number to check
+ * Return: square root of a number
+ */
+
+int check(int x, int base)
+{
+	if (x * x == base)
+		return (x);
+	if (x * x > base)
+		return (-1);
+	return (checker(x + 1, base));
+}
+
+/**
+ * _sqrt_recursion - return the natural square root of a number n.
+ * @n: number to check for square roots.
+ * Return: the natural square root of number n
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (checker(1, n));
+}
