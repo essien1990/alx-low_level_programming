300483ff94b34c7b6b41c7110e2efe042bed904e
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-07-01 00:11:18 -0700

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * _print - moves a string one place to the left and prints the string
  * @str: string to move
@@ -7,9 +8,11 @@
  *
  * Return: void
  */
+
 void _print(char *str, int l)
 {
 	int i, j;
+
 	i = j = 0;
 	while (i < l)
 	{
@@ -22,6 +25,7 @@ void _print(char *str, int l)
 	_putchar('\n');
 	free(str);
 }
+
 /**
  * mul - multiplies a char with a string and places the answer into dest
  * @n: char to multiply
@@ -29,12 +33,14 @@ void _print(char *str, int l)
  * @num_index: last non NULL index of num
  * @dest: destination of multiplication
  * @dest_index: highest index to start addition
- *
  * Return: pointer to dest, or NULL on failure
  */
+
+
 char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
 	int j, k, mul, mulrem, add, addrem;
+
 	mulrem = addrem = 0;
 	for (j = num_index, k = dest_index; j >= 0; j--, k--)
 	{
@@ -56,15 +62,19 @@ char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 	}
 	return (dest);
 }
+
+
 /**
  * check_for_digits - checks the arguments to ensure they are digits
  * @av: pointer to arguments
- *
  * Return: 0 if digits, 1 if not
  */
+
+
 int check_for_digits(char **av)
 {
 	int i, j;
+
 	for (i = 1; i < 3; i++)
 	{
 		for (j = 0; av[i][j]; j++)
@@ -75,33 +85,40 @@ int check_for_digits(char **av)
 	}
 	return (0);
 }
+
+
 /**
  * init - initializes a string
  * @str: sting to initialize
  * @l: length of strinf
- *
  * Return: void
  */
+
+
 void init(char *str, int l)
 {
 	int i;
+
 	for (i = 0; i < l; i++)
 		str[i] = '0';
 	str[i] = '\0';
 }
+
 /**
  * main - multiply two numbers
  * @argc: number of arguments
  * @argv: argument vector
- *
  * Return: zero, or exit status of 98 if failure
  */
+
 int main(int argc, char *argv[])
 {
 	int l1, l2, ln, ti, i;
+
 	char *a;
 	char *t;
 	char e[] = "Error\n";
+
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
