6186f95b59fb8928d2b98d92130e25c93886a061
Felix Adeh Essienne <nanaitconsult@gmail.com>
2022-07-07 19:00:50 -0700

    0x10. C - Variadic functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,6 +7,8 @@
   * @...: variable list of arguments
   * Return: sum of all arguments
   */
+
+
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
@@ -17,9 +19,7 @@ int sum_them_all(const unsigned int n, ...)
 		return (0);
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
-	{
 		result += va_arg(ap, int);
-	}
 	va_end(ap);
 	return (result);
 }
